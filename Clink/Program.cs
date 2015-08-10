606940d694825bf685361a4380b02535172871cb
Jesse Barocio <jesse@barocio.net>
2015-08-10 15:08:08 -0600

    Initial commit.

---
+++ Clink/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clink
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+        }
+    }
+}
